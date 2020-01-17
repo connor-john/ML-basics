bb59b72763b815c6cc619e5ba402eb7779bfaa99
Connor-John <connor.taylor@icloud.com>
2020-01-17 21:01:06 +1300

    update kmeans
    added url for dataset since this example is dependent on data

--- Basics/Clustering/kmeans.py
+++ Basics/Clustering/kmeans.py
@@ -12,6 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# For data set go to https://www.superdatascience.com/pages/machine-learning
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
