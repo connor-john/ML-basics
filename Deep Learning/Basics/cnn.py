bfc2db71aaf62f5bd5eddf86962cc644a9bc3cdb
Connor-John <connor.taylor@icloud.com>
2020-02-11 20:48:29 +1300

    updated CNN
    - added prediction component

--- Deep Learning/Basics/cnn.py
+++ Deep Learning/Basics/cnn.py
@@ -69,3 +69,35 @@ classifier.fit_generator(
         workers = 4
         )
 
+
+# Making a prediction
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indeces
+if result[0][0] == 1:
+    prediction = 'Dog'
+else:
+    prediction = 'Cat'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
