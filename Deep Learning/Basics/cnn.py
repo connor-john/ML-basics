7a4ef3f48f6db71bf01ac1ca3c6edac1ac759a50
Connor-John <connor.taylor@icloud.com>
2020-02-12 21:08:21 +1300

    Tuned CNN
    - Improved CNN performance

--- Deep Learning/Basics/cnn.py
+++ Deep Learning/Basics/cnn.py
@@ -10,10 +10,7 @@ Created on Thu Jan 30 19:54:34 2020
 
 # Importing
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Convolution2D
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Convolution2D, Dropout, Flatten, Dense, MaxPooling2D, BatchNormalization
 
 #Change file path root
 file_path = ''
@@ -22,21 +19,33 @@ file_path = ''
 classifier = Sequential()
 
 # Convolutional Layer
-classifier.add(Convolution2D(32, (3, 3), input_shape=(64, 64, 3), activation = 'relu'))
+classifier.add(Convolution2D(32, (3, 3), input_shape=(180, 180, 3), activation = 'relu'))
 
 # Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(BatchNormalization())
 
 # Add another Convolutional Layer
-classifier.add(Convolution2D(32, (3, 3), activation = 'relu'))
+classifier.add(Convolution2D(64, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(BatchNormalization())
+
+# Add another Convolutional Layer
+classifier.add(Convolution2D(128, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(BatchNormalization())
 
 # Flattening
 classifier.add(Flatten())
 
 # Full Connection
-classifier.add(Dense(128, activation = 'relu'))
-classifier.add(Dense(1, activation = 'sigmoid'))
+classifier.add(Dense(128, activation = 'relu', kernel_initializer='uniform'))
+classifier.add(Dropout(0.4))
+classifier.add(Dense(units=96,activation='relu',kernel_initializer='uniform'))
+classifier.add(Dropout(0.3))
+classifier.add(Dense(units=64,activation='relu',kernel_initializer='uniform'))
+classifier.add(Dropout(0.2))
+classifier.add(Dense(1, activation = 'sigmoid',kernel_initializer='uniform'))
 
 # Compile
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
@@ -54,41 +63,41 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
         file_path + 'dataset/training_set',
-        target_size=(64, 64),
-        batch_size=40,
+        target_size=(180, 180),
+        batch_size=128,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         file_path + 'dataset/test_set',
-        target_size=(64, 64),
-        batch_size=40,
+        target_size=(180, 180),
+        batch_size=128,
         class_mode='binary')
 
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=200,
-        epochs=25,
+        steps_per_epoch=8000/128,
+        epochs=100,
         validation_data=test_set,
-        validation_steps=50,
+        validation_steps=2000/128,
         workers = 4
         )
 
 print('Model finished')
 
-# Making a prediction
-import numpy as np
-from tensorflow.keras.preprocessing import image
-test_image = image.load_img(file_path + 'dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-training_set.class_indeces
-if result[0][0] == 1:
-    prediction = 'Dog'
-else:
-    prediction = 'Cat'
-
-print('The prediction was ' + prediction)
+## Making a prediction
+#import numpy as np
+#from tensorflow.keras.preprocessing import image
+#test_image = image.load_img(file_path + 'dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64, 64))
+#test_image = image.img_to_array(test_image)
+#test_image = np.expand_dims(test_image, axis = 0)
+#result = classifier.predict(test_image)
+#training_set.class_indeces
+#if result[0][0] == 1:
+#    prediction = 'Dog'
+#else:
+#    prediction = 'Cat'
+#
+#print('The prediction was ' + prediction)
 
 
 
