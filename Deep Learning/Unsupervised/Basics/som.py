1fc944ccc25978fe4d374ce2e1e398887659007b
Connor-John <connor.taylor@icloud.com>
2020-02-24 21:40:36 +1300

    updated som
    - wrong file

--- Deep Learning/Unsupervised/Basics/som.py
+++ Deep Learning/Unsupervised/Basics/som.py
@@ -1,79 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 28 19:57:19 2020
+Created on Mon Feb 24 20:07:13 2020
 
 @author: Connor
 """
 
-# Artificial Neural Network
+# Self Organising Map (SOM)
 
-# Importing the libraries
+# Import libraries
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('C:/Users/Connor/Documents/Python/Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13 ].values
+# Importing the dataset (Using UCI Machine learning Repository)
+dataset = pd.read_csv("Credit_Card_Applications.csv")
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, -1].values 
 
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-transformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1])], remainder = 'passthrough')
-X = np.array(transformer.fit_transform(X), dtype=np.float)
-X = X[:, 1:]
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Feature Scaling (Very Important)
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Tuning the model
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier 
-from sklearn.model_selection import GridSearchCV
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-def build_classifier(optimizer):
-    classifier = Sequential()
-    classifier.add(Dense(6, kernel_initializer='random_uniform', activation = 'relu', input_shape = (11,)))
-    classifier.add(Dense(6, kernel_initializer='random_uniform', activation = 'relu'))
-    classifier.add(Dense(1, kernel_initializer='random_uniform', activation = 'sigmoid'))
-    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
-    return classifier
-
-classifier = KerasClassifier(build_fn = build_classifier)
-parameters = {'batch_size': [25, 32],
-              'epochs': [100, 500],
-              'optimizer': ['adam', 'rmsprop']}
-grid_search = GridSearchCV(estimator = classifier, param_grid = parameters, scoring = 'accuracy', cv = 10, n_jobs = 4)
-grid_search = grid_search.fit(X_train, y_train)
-best_param = grid_search.best_params_
-best_acc = grid_search.best_score_
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Training the SOM (Using minisom https://github.com/JustGlowing/minisom/blob/master/minisom.py)
+from minisom import MiniSom
+sigma = 1.0
+learning_rate = 0.5
+som = MiniSom(x = 10, y = 10, input_len = np.size(X, 1), sigma = sigma, learning_rate = learning_rate)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
