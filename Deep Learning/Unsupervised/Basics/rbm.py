caf003ffeaead61059ef50bb7494a64a43e3698b
Connor-John <connor.taylor@icloud.com>
2020-02-29 20:39:35 +1300

    Completed RBM
    train loss = 0.24 on first attempt

--- Deep Learning/Unsupervised/Basics/rbm.py
+++ Deep Learning/Unsupervised/Basics/rbm.py
@@ -64,9 +64,66 @@ test_set[test_set == 2] = 0
 test_set[test_set >= 3] = 1
 
 # Creating the architecture of NN
+class RBM():
+    def __init__(self, nv, nh):
+        self.W = torch.randn(nh, nv)
+        self.a = torch.randn(1, nh)
+        self.b = torch.randn(1, nv)
+    
+    def sample_h(self, x):
+        wx = torch.mm(x, self.W.t())
+        activation = wx + self.a.expand_as(wx)
+        p_h_given_v = torch.sigmoid(activation)
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
 
+    def sample_v(self, y):
+        wy = torch.mm(y, self.W)
+        activation = wy + self.b.expand_as(wy)
+        p_v_given_h = torch.sigmoid(activation)
+        return p_v_given_h, torch.bernoulli(p_v_given_h)
+    
+    def train(self, v0, vk, ph0, phk):
+        self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
+        self.b += torch.sum((v0 - vk), 0)
+        self.a += torch.sum((ph0 - phk), 0)
+    
+# Initialising the RBM
+nv = len(training_set[0])
+nh = 100
+batch_size = 100
+rbm = RBM(nv, nh)
 
+# Training the RBM
+nb_epoch = 10
+for epoch in range(1, nb_epoch + 1):
+    train_loss = 0
+    s = 0.
+    for id_user in range(0, nb_users - batch_size, batch_size):
+        vk = training_set[id_user : id_user + batch_size]
+        v0 = training_set[id_user : id_user + batch_size]
+        ph0,_ = rbm.sample_h(v0)
+        for k in range(10):
+            _,hk = rbm.sample_h(vk)
+            _,vk = rbm.sample_v(hk)
+            vk[v0 < 0] = v0[v0 < 0]
+        phk,_ = rbm.sample_h(vk)
+        rbm.train(v0, vk, ph0, phk)
+        train_loss += torch.mean(torch.abs(v0[v0 >= 0] - vk[v0 >= 0]))
+        s += 1.
+    print('epoch: ' + str(epoch) + ' | loss: ' + str(train_loss/s))
 
+# Testing the RBM
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    v = training_set[id_user : id_user + 1]
+    vt = test_set[id_user : id_user + 1]
+    if len(vt[vt>=0]) > 0:
+        _,h = rbm.sample_h(v)
+        _,v = rbm.sample_v(h)
+        test_loss += torch.mean(torch.abs(vt[vt >= 0] - v[vt >= 0]))
+        s += 1.
+print('test_loss ' + str(test_loss/s))
 
 
 
